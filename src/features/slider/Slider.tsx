b13fe169e5e30fc1edab303608b0b781e09c132f
imelos <imelose@gmail.com>
2024-03-01 00:06:35 +0400

    +

--- src/features/slider/Slider.tsx
+++ src/features/slider/Slider.tsx
@@ -1,48 +1,57 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View, Image} from 'react-native';
+import {StyleSheet, Text, View, Image, Dimensions} from 'react-native';
 import {SliderParams} from '@src/navigation/Navigation';
 import Slider from '@react-native-community/slider';
 
+const windowWidth = Dimensions.get('window').width;
+const imageWidth = windowWidth * 0.8;
+
 const SliderComponent: React.FC<SliderParams> = ({photos}) => {
-  const [width, setWidth] = useState(0);
+  const [width, setWidth] = useState<number>(0);
+
+  const displayMessage = (value: number) => {
+    if (value === 0) {
+      console.log('after');
+    }
+    if (value >= imageWidth) {
+      console.log('before');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>{photos.after.modificationDate}</Text>
       <View style={{position: 'relative'}}>
         <Image
           style={{
-            width: 200,
-            height: 200,
+            width: imageWidth,
+            aspectRatio: '0.75',
           }}
           source={{uri: photos.before.path}}
         />
         <View
           style={{
             width: width,
-            height: 200,
             overflow: 'hidden',
             position: 'absolute',
           }}>
           <Image
             style={{
-              width: 200,
-              height: 200,
+              width: imageWidth,
+              aspectRatio: '0.75',
             }}
             source={{uri: photos.after.path}}
           />
         </View>
       </View>
       <Slider
-        style={{width: 200, height: 40}}
+        style={{width: '87%', height: 40}}
         minimumValue={0}
-        maximumValue={200}
-        minimumTrackTintColor="#FFFFFF"
+        maximumValue={imageWidth}
+        minimumTrackTintColor="purple"
         maximumTrackTintColor="#000000"
-        onValueChange={value => {
-          console.log('val change');
-          console.log(value);
-          setWidth(value);
-        }}
+        onValueChange={setWidth}
+        onSlidingComplete={displayMessage}
       />
     </View>
   );
@@ -52,6 +61,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     position: 'relative',
+    alignItems: 'center',
   },
 });
 
